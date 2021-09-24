38f37e91805c01d37cbb78faac3826844a708c98
carbowen <carbowen@iu.edu>
2021-09-24 00:37:15 -0400

    number
    my number

--- main.py
+++ main.py
@@ -15,9 +15,9 @@ def send_message(phone_number, message):
     os.system('osascript send.scpt {} "{}"'.format(phone_number, message))
 
 if __name__ == '__main__':
-    # put your phone number into the string! 
+    # put your phone number into the string!
     # if your number is (123)456-7890, the next line should look like this: phone_number = '1234567890'
-    phone_number = ''
+    phone_number = '7655850201'
     words = get_words('miracle.txt')
     for word in words:
         send_message(phone_number, word)
